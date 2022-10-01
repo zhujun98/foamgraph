bd05517cef296330f50014bf4082cb7e2aef8dbe
Jun Zhu <zhujun981661@gmail.com>
2022-10-01 13:30:45 +0200

    Clean up GraphicsItem

--- foamgraph/pyqtgraph_be/graphicsItems/GraphicsItem.py
+++ foamgraph/pyqtgraph_be/graphicsItems/GraphicsItem.py
@@ -1,12 +1,96 @@
-import warnings
+import itertools
 from functools import reduce
+
 from ..Qt import QtGui, QtCore, isQObjectAlive
 from ..GraphicsScene import GraphicsScene
 from ..Point import Point
 from .. import functions as fn
 import weakref
 import operator
-from ..util.lru_cache import LRUCache
+
+
+class LRUCache(object):
+    '''
+    This LRU cache should be reasonable for short collections (until around 100 items), as it does a
+    sort on the items if the collection would become too big (so, it is very fast for getting and
+    setting but when its size would become higher than the max size it does one sort based on the
+    internal time to decide which items should be removed -- which should be Ok if the resizeTo
+    isn't too close to the maxSize so that it becomes an operation that doesn't happen all the
+    time).
+    '''
+
+    def __init__(self, maxSize=100, resizeTo=70):
+        '''
+        ============== =========================================================
+        **Arguments:**
+        maxSize        (int) This is the maximum size of the cache. When some
+                       item is added and the cache would become bigger than
+                       this, it's resized to the value passed on resizeTo.
+        resizeTo       (int) When a resize operation happens, this is the size
+                       of the final cache.
+        ============== =========================================================
+        '''
+        assert resizeTo < maxSize
+        self.maxSize = maxSize
+        self.resizeTo = resizeTo
+        self._counter = 0
+        self._dict = {}
+        self._nextTime = itertools.count(0).__next__
+
+    def __getitem__(self, key):
+        item = self._dict[key]
+        item[2] = self._nextTime()
+        return item[1]
+
+    def __len__(self):
+        return len(self._dict)
+
+    def __setitem__(self, key, value):
+        item = self._dict.get(key)
+        if item is None:
+            if len(self._dict) + 1 > self.maxSize:
+                self._resizeTo()
+
+            item = [key, value, self._nextTime()]
+            self._dict[key] = item
+        else:
+            item[1] = value
+            item[2] = self._nextTime()
+
+    def __delitem__(self, key):
+        del self._dict[key]
+
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def clear(self):
+        self._dict.clear()
+
+    def values(self):
+        return [i[1] for i in self._dict.values()]
+
+    def keys(self):
+        return [x[0] for x in self._dict.values()]
+
+    def _resizeTo(self):
+        ordered = sorted(self._dict.values(), key=operator.itemgetter(2))[:self.resizeTo]
+        for i in ordered:
+            del self._dict[i[0]]
+
+    def items(self, accessTime=False):
+        '''
+        :param bool accessTime:
+            If True sorts the returned items by the internal access time.
+        '''
+        if accessTime:
+            for x in sorted(self._dict.values(), key=operator.itemgetter(2)):
+                yield x[0], x[1]
+        else:
+            for x in self._dict.items():
+                yield x[0], x[1]
 
 
 class GraphicsItem(object):
@@ -34,7 +118,7 @@ class GraphicsItem(object):
     _pixelVectorGlobalCache = LRUCache(100, 70)
     _mapRectFromViewGlobalCache = LRUCache(100, 70)
 
-    def __init__(self, register=None):
+    def __init__(self):
         if not hasattr(self, '_qtBaseClass'):
             for b in self.__class__.__bases__:
                 if issubclass(b, QtGui.QGraphicsItem):
@@ -48,11 +132,7 @@ class GraphicsItem(object):
         self._viewBox = None
         self._connectedView = None
         self._exportOpts = False   ## If False, not currently exporting. Otherwise, contains dict of export options.
-        if register is not None and register:
-            warnings.warn(
-                "'register' argument is deprecated and does nothing",
-                DeprecationWarning, stacklevel=2
-            )
+        self._cachedView = None
 
     def getViewWidget(self):
         """
@@ -153,7 +233,7 @@ class GraphicsItem(object):
             p = p.parentItem()
             if p is None:
                 break
-            if p.flags() & self.ItemClipsChildrenToShape:
+            if p.flags() & self.GraphicsItemFlag.ItemClipsChildrenToShape:
                 parents.append(p)
         return parents
     
@@ -267,7 +347,7 @@ class GraphicsItem(object):
         Return None if pixel size is not yet defined (usually because the item has not yet been displayed).
         """
         normV, orthoV = self.pixelVectors(direction)
-        if normV == None or orthoV == None:
+        if normV is None or orthoV is None:
             return None
         if ortho:
             return orthoV.length()
@@ -528,7 +608,6 @@ class GraphicsItem(object):
     
     def childrenShape(self):
         """Return the union of the shapes of all descendants of this item in local coordinates."""
-        childs = self.allChildItems()
         shapes = [self.mapFromItem(c, c.shape()) for c in self.allChildItems()]
         return reduce(operator.add, shapes)
     
