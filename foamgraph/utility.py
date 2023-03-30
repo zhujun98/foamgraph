6a577c49e96efbc7ea487eeb20c09141797f1771
Jun Zhu <zhujun981661@gmail.com>
2023-03-30 22:32:27 +0200

    Change the way of calculating log scale in plot items
    Move PlotItem.toLogScale to utility

--- foamgraph/utility.py
+++ foamgraph/utility.py
@@ -5,6 +5,9 @@ The full license is in the file LICENSE, distributed with this software.
 
 Author: Jun Zhu
 """
+from typing import Optional
+
+import numpy as np
 
 
 def parse_boundary(text):
@@ -214,3 +217,13 @@ def normalize_angle(angle: float) -> float:
     while angle > 180:
         angle -= 360
     return angle
+
+
+def array_to_log_scale(arr: np.ndarray, epsilon: Optional[float] = None)\
+        -> np.ndarray:
+    """Convert array result to logarithmic scale."""
+    ret = np.nan_to_num(arr)
+    if epsilon is None:
+        epsilon = 1e-3
+    ret[ret <= epsilon] = epsilon
+    return np.log10(ret)
